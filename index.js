0413e5373cb68d942f8081f16f83b8796c79f100
Israelsoarex <122483799+Israelsoarex@users.noreply.github.com>
2024-02-17 10:13:09 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let dataIpt = document.querySelector("#dataIpt")
+let dataIpt = document.querySelector("#dataIpt");
 let btn = document.querySelector("#btn");
 let tipo = document.querySelector("#tipo");
 let slImg = document.querySelector('#slImg');
@@ -16,7 +16,7 @@ btn.addEventListener("click", ()=>{
     let diaControlado = diferencaDia + 30;
     switch (tipo.value) {
         case '10' :
- calAtbiotico(diferencaDia,agora,diferencaDia)
+ calAtbiotico(diferencaDia,agora,diferencaDia);
             break;
         case '30' : 
             
@@ -31,6 +31,12 @@ function calAtbiotico(x,agora,diferencaDia) {
    console.log(diaBiotico)
    const auxData = new Date();
    auxData.setDate(diaBiotico + agora.getDate())
-   
-    slPara.innerText = `Do dia ${agora.getDate()-(diferencaDia*-1)} até o dia ${agora.getDate()} já se passaram-se ${diferencaDia*-1} dias. Essa receita é valida ate o dia ${auxData.getDate()}/${auxData.getMonth()}/${auxData.getFullYear()}`;
+    if((diferencaDia*-1) <= 10) {
+        console.log("ta de boa")
+        slPara.innerText = `Do dia ${agora.getDate()-(diferencaDia*-1)} até o dia ${agora.getDate()} já se passaram-se ${diferencaDia*-1} dias. Essa receita é valida até o dia ${auxData.getDate()}/${auxData.getMonth().toString().padStart(2, '0')}/${auxData.getFullYear()}`;
+        slImg.src = `img/right.svg`;
+    }else {
+        slPara.innerText = `Do dia ${agora.getDate()-(diferencaDia*-1)} até o dia ${agora.getDate()} já se passaram-se ${diferencaDia*-1} dias. Essa receita venceu dia ${auxData.getDate()}/${auxData.getMonth().toString().padStart(2, '0')}/${auxData.getFullYear()}`;
+        slImg.src = `img/wrong.svg`;
+    }
 }
